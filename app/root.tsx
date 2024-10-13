c2dc9afd996007db631982ad3b675729309fd001
Sekiju <zzzcxk@gmail.com>
2024-10-13 23:28:10 +0500

    feat: remix ssr

---
+++ app/root.tsx
@@ -0,0 +1,28 @@
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
+import "./index.css"
+import { Toaster } from "~/components/ui/toaster"
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.png" sizes="any" type="image/png" />
+      <script defer data-domain="cfg.yor.ovh" src="https://a.shd.llc/js/script.js" />
+      <Meta />
+      <Links />
+    </head>
+    <body>
+    {children}
+    <ScrollRestoration />
+    <Scripts />
+    <Toaster />
+    </body>
+    </html>
+  )
+}
+
+export default function App() {
+  return <Outlet />
+}
